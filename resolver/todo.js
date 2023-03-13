4dfdcf351a61c9e74e029769d00510e8e452f8a1
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-03-13 23:02:06 +0300

    Implemented todo query & resolver

--- resolver/todo.js
+++ resolver/todo.js
@@ -2,6 +2,9 @@ module.exports = {
     Query: {
         async todos(_, __, { model }) {
             return await model.find({});
+        },
+        async todo(_, { id }, { model }) {
+            return await model.findOne({ _id: id })
         }
     },
 }
--- schema/todo.js
+++ schema/todo.js
@@ -16,6 +16,7 @@ const typeDefs = gql`
     
     type Query {
         todos: [Todo]!
+        todo(id: ID!): Todo!
     }
 `
 
